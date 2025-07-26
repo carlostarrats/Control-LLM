dbb706687e41f0f1b014a951103f4bc91026410c
Carlos Tarrats <carlostarrats@icloud.com>
2025-07-25 20:37:41 -0700

    Initial commit

---
+++ Control LLM/Control_LLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Control_LLMApp.swift
+//  Control LLM
+//
+//  Created by Carlos Tarrats on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Control_LLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Control LLMTests/Control_LLMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Control_LLMTests.swift
+//  Control LLMTests
+//
+//  Created by Carlos Tarrats on 7/25/25.
+//
+
+import Testing
+@testable import Control_LLM
+
+struct Control_LLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
